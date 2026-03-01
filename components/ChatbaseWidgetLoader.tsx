4256840040b83abd9509984a5ae8ec50ca4f000a
HeruAhmose <aitconsult22@gmail.com>
2026-02-28 20:47:51 -0500

    fix: remove Chatbase script, clean up hk-temp

--- components/ChatbaseWidgetLoader.tsx
+++ components/ChatbaseWidgetLoader.tsx
@@ -1,16 +1,1 @@
-"use client";
-import { useEffect } from "react";
-
-export default function ChatbaseWidgetLoader() {
-  useEffect(() => {
-    const BOT_ID = "3ylVDc3243y5659rIjvcd";
-    if (document.getElementById(BOT_ID)) return;
-    const s = document.createElement("script");
-    s.src = "https://www.chatbase.co/embed.min.js";
-    s.id = BOT_ID;
-    s.setAttribute("domain", "www.chatbase.co");
-    s.defer = true;
-    document.body.appendChild(s);
-  }, []);
-  return null;
-}
+"use client"; export default function ChatbaseWidgetLoader() { return null; }
